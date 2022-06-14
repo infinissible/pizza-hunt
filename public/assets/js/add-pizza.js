4695f65c18de625b31be8d62d24c37045d03cbb7
Seungjin <infinissible@gmail.com>
2022-06-13 18:29:19 -0700

    CRUD

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -2,7 +2,7 @@ const $addToppingBtn = document.querySelector('#add-topping');
 const $pizzaForm = document.querySelector('#pizza-form');
 const $customToppingsList = document.querySelector('#custom-toppings-list');
 
-const handleAddTopping = event => {
+const handleAddTopping = (event) => {
   event.preventDefault();
 
   const toppingValue = document.querySelector('#new-topping').value;
@@ -15,17 +15,11 @@ const handleAddTopping = event => {
   checkbox.type = 'checkbox';
   checkbox.name = 'topping';
   checkbox.value = toppingValue;
-  checkbox.id = toppingValue
-    .toLowerCase()
-    .split(' ')
-    .join('-');
+  checkbox.id = toppingValue.toLowerCase().split(' ').join('-');
 
   const label = document.createElement('label');
   label.textContent = toppingValue;
-  label.htmlFor = toppingValue
-    .toLowerCase()
-    .split(' ')
-    .join('-');
+  label.htmlFor = toppingValue.toLowerCase().split(' ').join('-');
 
   const divWrapper = document.createElement('div');
 
@@ -36,13 +30,15 @@ const handleAddTopping = event => {
   toppingValue.value = '';
 };
 
-const handlePizzaSubmit = event => {
+const handlePizzaSubmit = (event) => {
   event.preventDefault();
 
   const pizzaName = $pizzaForm.querySelector('#pizza-name').value;
   const createdBy = $pizzaForm.querySelector('#created-by').value;
   const size = $pizzaForm.querySelector('#pizza-size').value;
-  const toppings = [...$pizzaForm.querySelectorAll('[name=topping]:checked')].map(topping => {
+  const toppings = [
+    ...$pizzaForm.querySelectorAll('[name=topping]:checked'),
+  ].map((topping) => {
     return topping.value;
   });
 
@@ -51,6 +47,23 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+
+  fetch('/api/pizzas', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then((response) => response.json())
+    .then((postResponse) => {
+      alert('Pizza created successfully!');
+      console.log(postResponse);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
